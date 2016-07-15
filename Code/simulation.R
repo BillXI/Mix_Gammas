6c5c9ef2f95812b21fcb1fcf35a04e76ea570e0e
Bill CX <billchenxi@gmail.com>
2016-07-15 12:15:57 -0400

    Change simulation 1

--- Code/simulation.R
+++ Code/simulation.R
@@ -1,6 +1,6 @@
-#setwd("~/Documents/Git/Mix_Gammas/Code")
+setwd("~/Documents/Git/Mix_Gammas/Code")
 #setwd("/home/xch234/mix_gammas/Mix_Gammas/Code")
-source("./gammamixEM2.R")
+source("gammamixEM2.R")
 set.seed(518)
 #library("mixtools")
 library("MASS")
@@ -60,7 +60,7 @@ sample.generation <- function(sample.size, parameters){
         return(unlist(samples))
 }
 
-sample <- sample.generation(100, conditions[["C12"]])
+# sample <- sample.generation(100, conditions[["C12"]])
 
 ###################################
 estimation1.f <- function(dat, para){
@@ -72,72 +72,29 @@ estimation1.f <- function(dat, para){
         return(output[2:4])
 }
 
-estimation2.f <- function(dat, para){
-        numOfDist <- nrow(para)
-        output <- gammamixEM2(dat, k = numOfDist, epsilon = 0.1)
-        return(output[2:4])
-}
-
-estimation3.f <- function(dat, para){
-        datCubeRoot <- dat^(1/3)
-        numOfDist <- nrow(para)
-        classification <- normalmixEM(datCubeRoot, k = numOfDist) ## The classified data are clustered and not like real classified.
-        clf.dat <- apply((classification$posterior == T), 2, function(x) dat[x] )
-        para.est <- lapply(c(1:numOfDist), function(i){
-                test = try(nr.gamma(x=unlist(clf.dat[i]), eps = 0.01), silent = T)
-                if (class(test)=="try-error"){
-                        test = as.numeric(gammamix.init(unlist(clf.dat[i]), lambda = 1, k = 1)[2:3])                
-                        return(test)}
-                else{return(test$theta)}
-        }) 
-        a <- unlist(data.frame(para.est)[1,])
-        b <- unlist(data.frame(para.est)[2,])
-        l <- apply((classification$posterior == T), 2, function(x) {table(x)["TRUE"]/length(x)} )
-        output <- gammamixEM2(dat, lambda = l, alpha = a, beta = b, k = numOfDist)[2:4]
-        return(output)
-}
-
-estimation4.f <- function(dat, para){
-        datCubeRoot <- dat^(1/3)
-        numOfDist <- nrow(para)
-        classification <- normalmixEM(datCubeRoot, k = numOfDist)
-        clf.dat <- apply((classification$posterior == T), 2, function(x) dat[x] )
-        para.est <- lapply(c(1:numOfDist), function(i){
-                test = try(nr.gamma(x=unlist(clf.dat[i]), eps = 0.01), silent = T)
-                if (class(test)=="try-error"){
-                        test = as.numeric(gammamix.init(unlist(clf.dat[i]), lambda = 1, k = 1)[2:3])                
-                        return(test)}
-                else{return(test$theta)}
-        }) 
-        a <- para$a
-        b <- unlist(data.frame(para.est)[2,])
-        l <- apply((classification$posterior == T), 2, function(x) {table(x)["TRUE"]/length(x)} )
-        output <- gammamixEM2(dat, lambda = l, alpha = a, beta = b, k = numOfDist, fix.alpha = T)[2:4]
-        return(output)
-}
-
 #################################
 library(parallel)
-simulation <- function(s.size, condition, strategy){
+simulation <- function(s.size, cond, strategy){
         # 12 conditions
-        results <- lapply(condition, function(para){
+        results <- lapply(cond, function(para){
                 # 3 sample size
-                # print(para)
+                #print(para)
                 one.iteration.results <- lapply(s.size, function(s){
                         #print(s)
                         dat <- sample.generation(sample.size = s, parameters = para)
                         one.sample.result <- strategy(dat, para)
                         return(one.sample.result)
                 })
-                return(one.iteration.results)
+                #return(one.iteration.results)
         })        
         return(results)
 }
 
 sim1 <- mclapply( (1:n.iter), function(i){
-        results <- simulation(sample.size, conditions, estimation1.f)
+        results <- simulation(s.size = sample.size, cond = conditions, strategy = estimation1.f)
+        #print(results)
         return(results)
-}, mc.cores=8)
+}, mc.cores=1)
 
 exportJson <- toJSON(sim1)
 write(exportJson, "sim1.json")
