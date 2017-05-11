a795a4b2ff486de9a35a2b9b22a666b73b937761
Bill CX <billchenxi@gmail.com>
2017-05-11 12:21:59 -0400

    sh

---
+++ GenerateSH.R
@@ -0,0 +1,21 @@
+b = "#!/bin/bash
+#SBATCH --nodes=1 --ntasks-per-node=1 --mem=2G 
+#SBATCH -t 0-00:05
+#SBATCH --mail-user=user@uky.edu
+#SBATCH --mail-type=ALL
+
+# change dir to your working dir
+
+cd /home/xch234/mix_gammas/Mix_Gammas/Code/2017_15_11/"
+
+a = list.files("./Code/2017_5_11/")
+
+a = paste0("Rscript ", a)
+
+c = paste0(b, "\n", a)
+
+l = length(c)
+for(i in 1:l){
+        fname = paste0("sim",i,".sh")
+        write(c[i], file = paste0("./sh/",fname))
+}
---
+++ sh/sim10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1 --ntasks-per-node=1 --mem=2G 
+#SBATCH -t 0-00:05
+#SBATCH --mail-user=user@uky.edu
+#SBATCH --mail-type=ALL
+
+# change dir to your working dir
+
+cd /home/xch234/mix_gammas/Mix_Gammas/Code/2017_15_11/
+Rscript Condition_12_100.R
