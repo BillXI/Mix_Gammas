8118af704291163b151f02a219a37ab8bdc01c64
Bill CX <billchenxi@gmail.com>
2017-05-11 12:21:59 -0400

    sh

--- simulation.sh
+++ simulation.sh
@@ -1,60 +1,6 @@
 #!/bin/bash
-#SBATCH --nodes=1 --ntasks-per-node=1 --mem=2G --time=00:30:00
-#SBATCH --mail-user=xch234@uky.edu
-#SBATCH --mail-type=ALL
 
 
-
-
-
-# change dir to your working dir
-
-cd /home/xch234/mix_gammas/Mix_Gammas/Code/2017_15_11/
-
-# run the code `test.R`
-Rscript Condition_1_100.R
-Rscript Condition_1_250.R
-Rscript Condition_1_500.R
-Rscript Condition_10_100.R
-Rscript Condition_10_250.R
-Rscript Condition_10_500.R
-Rscript Condition_11_100.R
-Rscript Condition_11_250.R
-Rscript Condition_11_500.R
-Rscript Condition_12_100.R
-Rscript Condition_12_250.R
-Rscript Condition_12_500.R
-Rscript Condition_13_100.R
-Rscript Condition_13_250.R
-Rscript Condition_13_500.R
-Rscript Condition_14_100.R
-Rscript Condition_14_250.R
-Rscript Condition_14_500.R
-Rscript Condition_15_100.R
-Rscript Condition_15_250.R
-Rscript Condition_15_500.R
-Rscript Condition_2_100.R
-Rscript Condition_2_250.R
-Rscript Condition_2_500.R
-Rscript Condition_3_100.R
-Rscript Condition_3_250.R
-Rscript Condition_3_500.R
-Rscript Condition_4_100.R
-Rscript Condition_4_250.R
-Rscript Condition_4_500.R
-Rscript Condition_5_100.R
-Rscript Condition_5_250.R
-Rscript Condition_5_500.R
-Rscript Condition_6_100.R
-Rscript Condition_6_250.R
-Rscript Condition_6_500.R
-Rscript Condition_7_100.R
-Rscript Condition_7_250.R
-Rscript Condition_7_500.R
-Rscript Condition_8_100.R
-Rscript Condition_8_250.R
-Rscript Condition_8_500.R
-Rscript Condition_9_100.R
-Rscript Condition_9_250.R
-Rscript Condition_9_500.R
-
+for filename in /sh/; do
+    qsub filename
+done
